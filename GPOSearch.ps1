50169bb13245c5198bd3d5040f60168b4286a898
CharlieLima0391 <charlielima0391@outlook.com>
2023-05-24 13:49:41 +0100

    Add files via upload

---
+++ GPOSearch.ps1
@@ -0,0 +1,17 @@
+﻿Import-module grouppolicy
+
+$gpos= get-gpo -all
+
+foreach ($object in $gpos){
+
+    $gpoSetting = get-gporeport -guid $object.id -ReportType xml | select-string -Pattern "cmd" #change the pattern to edit the search crieria
+    
+
+    if ($gpoSetting){
+        Write-output "GPO Name: $($object.displayname)"
+        Write-Output "GPO ID: $($object.id)"
+        Write-Output "------------------------------------------------------"
+
+        }
+
+    }
