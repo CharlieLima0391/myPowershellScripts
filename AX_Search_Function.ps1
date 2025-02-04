b3ec5aa630b14e40fd7876e36fdddd803d708d9a
CharlieLima0391 <charlielima0391@outlook.com>
2025-02-04 16:24:49 +0000

    Add files via upload

---
+++ AX_Search_Function.ps1
@@ -0,0 +1,33 @@
+﻿Import-Module ExchangeOnlineManagement
+
+#$EmailList = Get-Content C:\Users\WDAGUtilityAccount\Desktop\MailBoxList.txt
+
+
+$emailList = @(
+
+"email1@mail.com",
+"email2@mail.com",
+"email3@mail.com"
+
+)
+
+
+
+ForEach ($email in $EmailList){
+    
+    Write-Host "`nChecking Mailbox:" $email
+
+    $axRules = Get-InboxRule -Mailbox $email | Where-Object { $_.Name -like "*AX*"}
+
+    if ($axRules) {
+
+        Write-Host "Rules Containing 'AX' in the name:"
+        $axRules
+    }
+    else {
+    Write-Host "No Rules Match"
+    }
+
+    write-host " ----- Next ----- "
+
+}
